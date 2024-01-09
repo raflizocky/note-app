4da07c4871fc8cbd0072a7d874833a039687df99
Rafli Zocky Leonard <raflizocky@gmail.com>
2024-01-09 07:22:02 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Catatan Sederhana',
+      title: 'Notes App',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
         colorScheme: ColorScheme.fromSwatch(
@@ -19,7 +21,7 @@ class MyApp extends StatelessWidget {
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
