67b06e8b1d1c75e0dc1a6b48b292e29dcd774226
Rafli Zocky Leonard <raflizocky@gmail.com>
2024-01-09 07:21:39 +0700

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,74 +3,78 @@ import 'add_note_page.dart';
 import 'note_card.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({Key? key});
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   List<Map<String, String>> notes = [
-    {'note': 'Catatan 1', 'description': 'Deskripsi catatan 1'},
-    {'note': 'Catatan 2', 'description': 'Deskripsi catatan 2'},
+    {'note': 'Note 1', 'description': 'Description'},
+    {'note': 'Note 2', 'description': 'Description'},
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Catatan Sederhana'),
+        title: const Text('Notes App'),
       ),
       body: SafeArea(
-        child: Expanded(
-          child: ListView.builder(
-            itemCount: notes.length,
-            itemBuilder: (context, index) {
-              return NoteCard(
-                noteData: notes[index],
-                onDelete: () {
-                  setState(() {
-                    notes.removeAt(index);
-                  });
-                },
-                onEdit: () async {
-                  final editedNote = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AddNotePage(
-                        initialNote: notes[index]['note'],
-                        initialDescription: notes[index]['description'],
-                      ),
-                    ),
-                  );
-                  if (editedNote != null) {
-                    setState(() {
-                      notes[index]['note'] = editedNote['note']!;
-                      notes[index]['description'] = editedNote['description']!;
-                    });
-                  }
-                },
-              );
-            },
+        child: ListView.builder(
+          itemCount: notes.length,
+          itemBuilder: (context, index) => NoteCard(
+            noteData: notes[index],
+            onDelete: () => _deleteNote(index),
+            onEdit: () => _editNote(index),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final newNote = await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddNotePage()),
-          );
-          if (newNote != null) {
-            setState(() {
-              notes.add({
-                'note': newNote['note']!,
-                'description': newNote['description']!,
-              });
-            });
-          }
-        },
-        child: Icon(Icons.add),
+        onPressed: () => _addNote(),
         backgroundColor: Theme.of(context).colorScheme.secondary,
+        child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void _addNote() async {
+    final newNote = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const AddNotePage()),
+    );
+    if (newNote != null) {
+      setState(() {
+        notes.add({
+          'note': newNote['note']!,
+          'description': newNote['description']!,
+        });
+      });
+    }
+  }
+
+  void _editNote(int index) async {
+    final editedNote = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddNotePage(
+          initialNote: notes[index]['note'],
+          initialDescription: notes[index]['description'],
+        ),
+      ),
+    );
+    if (editedNote != null) {
+      setState(() {
+        notes[index]['note'] = editedNote['note']!;
+        notes[index]['description'] = editedNote['description']!;
+      });
+    }
+  }
+
+  void _deleteNote(int index) {
+    setState(() {
+      notes.removeAt(index);
+    });
   }
 }
